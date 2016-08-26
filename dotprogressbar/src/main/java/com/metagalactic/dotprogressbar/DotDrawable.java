6a56fe7aca3a42233b54bbf6f35ab1d96ca46812
Manij Shrestha <manijshrestha@gmail.com>
2016-08-25 19:44:29 -0500

    Added DotDrawable and updated DotProgressView along with attributes

---
+++ dotprogressbar/src/main/java/com/metagalactic/dotprogressbar/DotDrawable.java
@@ -0,0 +1,48 @@
+package com.metagalactic.dotprogressbar;
+
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.ColorInt;
+import android.support.annotation.IntRange;
+
+public class DotDrawable extends Drawable {
+
+    private Paint mPaint;
+    private float mRadius;
+
+    public DotDrawable(@ColorInt final int color, float radius) {
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        mPaint.setColor(color);
+        mRadius = radius;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        Rect bounds = getBounds();
+        canvas.drawCircle(bounds.centerX(), bounds.centerY(), mRadius, mPaint);
+    }
+
+    @Override
+    public void setAlpha(@IntRange(from = 0, to = 255) int alpha) {
+        mPaint.setAlpha(alpha);
+        invalidateSelf();
+    }
+
+    @Override
+    public void setColorFilter(ColorFilter colorFilter) {
+        mPaint.setColorFilter(colorFilter);
+        invalidateSelf();
+    }
+
+    @Override
+    public int getOpacity() {
+        return PixelFormat.TRANSLUCENT;
+    }
+}
--- dotprogressbar/src/main/java/com/metagalactic/dotprogressbar/DotProgressBar.java
+++ dotprogressbar/src/main/java/com/metagalactic/dotprogressbar/DotProgressBar.java
@@ -1,21 +1,107 @@
 package com.metagalactic.dotprogressbar;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.support.annotation.ColorInt;
+import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DotProgressBar extends View {
 
+    private List<DotDrawable> mDots;
+
+    @ColorInt
+    private int mDotColor;
+    private int mNoOfDots;
+    private float mDotRadius;
+    private float mDotSpacing;
+    private float mGrowFactor;
+
     public DotProgressBar(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public DotProgressBar(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public DotProgressBar(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(attrs);
     }
 
+    private void init(@Nullable AttributeSet attrs) {
+        Context context = getContext();
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.DotProgressBar);
+
+        try {
+            mNoOfDots = typedArray.getInt(R.styleable.DotProgressBar_dpb_noOfDots, 4);
+            mDotColor = typedArray.getColor(R.styleable.DotProgressBar_dpb_dotColor, Color.GRAY);
+            mDotRadius = typedArray.getDimension(R.styleable.DotProgressBar_dpb_dotRadius, 18);
+            mGrowFactor = typedArray.getFloat(R.styleable.DotProgressBar_dpb_growFactor, 1.5f);
+            mDotSpacing = typedArray.getDimension(R.styleable.DotProgressBar_dpb_dotSpacing, 8);
+        } finally {
+            typedArray.recycle();
+        }
+
+        mDots = new ArrayList<>(mNoOfDots);
+
+        setupDots();
+        adjustDotBounds();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        float dotSize = mDotRadius * 2;
+        float viewWidth = (dotSize * mNoOfDots) + (mDotSpacing * (mNoOfDots - 1));
+        setMeasuredDimension((int) viewWidth, (int) (dotSize * mGrowFactor));
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        for (int i = 0; i < mDots.size(); i++) {
+            mDots.get(i).draw(canvas);
+        }
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        adjustDotBounds();
+    }
+
+    private void adjustDotBounds() {
+        float dotSize = mDotRadius * 2;
+        float maxDotSize = dotSize * mGrowFactor;
+        float dotSizeWithSpace = dotSize + mDotSpacing;
+
+        int left = (int) (mDotRadius * (1 - mGrowFactor));
+        int right = left + (int) maxDotSize;
+        int top = 0;
+        int bottom = (int) maxDotSize;
+
+        for (int i = 0; i < mDots.size(); i++) {
+            DotDrawable dotDrawable = mDots.get(i);
+            dotDrawable.setBounds(left, top, right, bottom);
+
+            left += dotSizeWithSpace;
+            right += dotSizeWithSpace;
+        }
+    }
+
+    private void setupDots() {
+        mDots.clear();
+
+        for (int i = 0; i < mNoOfDots; i++) {
+            DotDrawable dotDrawable = new DotDrawable(mDotColor, mDotRadius);
+            dotDrawable.setCallback(this);
+            mDots.add(dotDrawable);
+        }
+    }
 }
